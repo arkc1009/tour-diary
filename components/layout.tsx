98033142cd65f4175c192e61564bf097baed037e
한동진 <arkc1009@gmail.com>
2022-07-15 09:31:04 +0900

    feat: add Layout

---
+++ components/layout.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+
+interface MenuItemType {
+  href: string;
+  title: string;
+}
+
+const Layout: React.FC = ({ children }) => {
+  const router = useRouter();
+
+  const menuItems: MenuItemType[] = useMemo(
+    () => [
+      { href: "/", title: "main" },
+      { href: "/about", title: "about" },
+    ],
+    []
+  );
+
+  return (
+    <div className="flex min-h-screen">
+      <div className="min-h-full w-64 bg-red-300 py-8 px-4">
+        <nav>
+          <ul className="flex flex-col gap-4">
+            {menuItems.map(({ href, title }) => (
+              <li key={title}>
+                <Link href={href}>
+                  <a
+                    className={`block h-full w-full cursor-pointer rounded bg-red-500 p-4 hover:bg-white ${
+                      router.asPath === href && "bg-white"
+                    }`}
+                  >
+                    {title}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className="h-full">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
